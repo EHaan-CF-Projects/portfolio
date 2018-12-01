6232f1a5bf5310135add908b810862bfd09086bf
EvyHaan <evy.haan@gmail.com>
2018-12-01 14:48:51 -0800

    begin to update photos

--- server.js
+++ server.js
@@ -14,13 +14,13 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('./public'));
 
 app.get('/hello', (request, response) => {
-    response.status(200).send('hello');
+  response.status(200).send('hello');
 });
 
 // app.get('home', res.send(index.html))
 
 app.use('*', (req, res) => {
-    res.status(400).send('sorry that did not work');
+  res.status(400).send('sorry that did not work');
 })
 
-app.listen(PORT, () => console.log(`listening on port ${PORT}`))   // should say 3000 (per line 12) if you don't tell it anywhere else
+app.listen(PORT, () => console.log(`listening on port ${PORT}`)) // should say 3000 (per line 12) if you don't tell it anywhere else
